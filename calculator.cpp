db4a9c10ebce9123df0ad72c6aa45adac9855c1a
SJPavithra <22bca079@psgrkcw.ac.in>
2024-12-29 17:01:55 +0530

    Create calculator.cpp

---
+++ calculator.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    char op;
+    double num1, num2;
+
+    cout << "Enter operator (+, -, *, /): ";
+    cin >> op;
+    cout << "Enter two numbers: ";
+    cin >> num1 >> num2;
+
+    switch (op) {
+        case '+': cout << num1 + num2; break;
+        case '-': cout << num1 - num2; break;
+        case '*': cout << num1 * num2; break;
+        case '/': cout << (num2 != 0 ? num1 / num2 : 0); break;
+        default: cout << "Invalid operator!"; break;
+    }
+
+    return 0;
+}
